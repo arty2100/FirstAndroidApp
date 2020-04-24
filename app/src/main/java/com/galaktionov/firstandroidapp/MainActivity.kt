d5f68a29a3425f03929b2afd39d291dfefd28799
art_spb@hotmail.it <Privet2100+>
2020-04-24 11:41:42 +0200

    k

---
+++ app/src/main/java/com/galaktionov/firstandroidapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.galaktionov.firstandroidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
